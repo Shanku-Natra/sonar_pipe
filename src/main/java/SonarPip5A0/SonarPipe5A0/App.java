c7f9aad865c88a72fd543627add4dee0d97860e9
shanku45 <shankunatra678@gmail.com>
2022-12-02 20:12:45 +0530

    sonar pipeline first commit

---
+++ src/main/java/SonarPip5A0/SonarPipe5A0/App.java
@@ -0,0 +1,13 @@
+package SonarPip5A0.SonarPipe5A0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SonarPip5A0/SonarPipe5A0/AppTest.java
@@ -0,0 +1,20 @@
+package SonarPip5A0.SonarPipe5A0;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
